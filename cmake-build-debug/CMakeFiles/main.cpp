f19c0f6b51a3c76238222e43a1bbdf80808f7c3a
rebeccaantobre <rebeccantobre@gmail.com>
2025-06-11 15:17:15 +0000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+ int baseSalary=1000;
+ float yearsWorked;
+ float bonus1;
+ float bonus2;
+ float totalSales;
+ double commission;
+ float totalSalary;
+ int main()
+ {
+     std::cout << "CSM 158 CHALLENGE 1" << std::endl;
+     std::cout << "Enter number of years worked." << std::endl;
+     std::cin >> yearsWorked;
+     std::cout << "Enter your total sales for the month" << std::endl;
+     std::cin >> totalSales;
+
+     if (yearsWorked<= 5)
+     {
+         bonus1=10*yearsWorked;
+     } else
+     {
+         bonus2=20*yearsWorked;
+     }
+
+     if (totalSales >= 5000 && totalSales < 10000)
+     {
+         commission= 0.03 * totalSales;
+     } else if (totalSales >= 10000)
+     {
+         commission= 0.06 * totalSales;
+     }
+     //calculate total salary
+     totalSalary= baseSalary + commission + bonus1 + bonus2;
+     std::cout << "Your total salary is:  " << totalSalary <<  std::endl;
+
+     return 0;
+ }
